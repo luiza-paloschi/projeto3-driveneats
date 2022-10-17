1c65b9f81a9c67d0fcc6963ffd9bae26de26aff4
luiza-paloschi <luizapaloschi@gmail.com>
2022-10-17 08:44:25 -0300

    finalizar pedido funcionando, envio do pedido no whatsapp

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let foodsel 
+let foodsel;
 let drinksel;
 let dessertsel;
 let pricefood;
@@ -6,12 +6,11 @@ let pricedrink;
 let pricedessert;
 
 
-   function criarícone(id){
+function criarícone(id){
    let eldiv = document.createElement('div');
    eldiv.className = 'icon'
    eldiv.id = id
    eldiv.innerHTML = '<ion-icon name="checkmark-circle"></ion-icon>'
-
    return eldiv
 }
 
@@ -23,13 +22,18 @@ function selectFood(food){
       let remove_icon = document.getElementById("ícone");
       remove_icon.parentNode.removeChild(remove_icon)
    }
+
    food.classList.add('selected');
    let divIcon = criarícone('ícone')
    food.appendChild(divIcon)
    foodsel = document.querySelector('.food_row .selected .card_title').innerHTML;
-
+   pricefood = document.querySelector('.food_row .selected .card_price').innerHTML;
+   pricefood = pricefood.replace(",", ".")
+   pricefood = Number(pricefood.replace('R$ ', ''))
+   if (verificar()){
+      mudabotão()
+   }
 }
-
 
 function selectDrink(drink){
    
@@ -43,7 +47,12 @@ function selectDrink(drink){
    let divIcon = criarícone('ícone1')
    drink.appendChild(divIcon)
    drinksel = document.querySelector('.drink_row .selected .card_title').innerHTML;
-
+   pricedrink = document.querySelector('.drink_row .selected .card_price').innerHTML;
+   pricedrink = pricedrink.replace(",", ".")
+   pricedrink = Number(pricedrink.replace('R$ ', ''))
+   if (verificar()){
+      mudabotão()
+   }
 }
 
 function selectDessert(dessert){
@@ -57,6 +66,47 @@ function selectDessert(dessert){
    let divIcon = criarícone('ícone2')
    dessert.appendChild(divIcon)
    dessertsel = document.querySelector('.dessert_row .selected .card_title').innerHTML;
+   pricedessert = document.querySelector('.dessert_row .selected .card_price').innerHTML;
+   pricedessert = pricedessert.replace(",", ".")
+   pricedessert = Number(pricedessert.replace('R$ ', ''))
+   if (verificar()){
+         mudabotão()
+   }
 }
 
+function verificar(){
+   let foodselecionada = document.querySelector('.food_row .selected');
+   let drinkselecionada = document.querySelector('.drink_row .selected');
+   let dessertselecionada = document.querySelector('.dessert_row .selected');
 
+   return (foodselecionada!== null) && (drinkselecionada !== null) && (dessertselecionada !== null)
+}
+
+function mudabotão(){
+   let botão = document.querySelector('.button')
+   let texto = document.querySelector('.button p')
+   botão.classList.add('finish')
+   texto.innerHTML = 'Fechar pedido'
+}
+
+function finalizarpedido(){
+  if (verificar()){
+   let link = document.querySelector('.link')
+   let valortotal = (calcularValor(pricefood, pricedrink, pricedessert))
+   let mensagem =`Olá, gostaria de fazer o pedido:\n- Prato: ${foodsel}\n- Bebida: ${drinksel}\n- Sobremesa: ${dessertsel}\nTotal: R$ ${valortotal}`;
+    let mensagemencode = encodeURIComponent(mensagem)
+    let url = 'https://wa.me/5554992066376?text='+ mensagemencode
+    console.log(mensagemencode)
+    link.setAttribute('href', url)
+  }
+      
+
+}
+
+function calcularValor(pricefood, pricedrink, pricedessert){
+  let valor = (pricefood + pricedrink + pricedessert).toFixed(2)
+  valor = valor.toString()
+  valor = valor.replace('.', ',')
+  return valor
+}
+   
